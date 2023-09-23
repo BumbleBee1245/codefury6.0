eaf8fe9a7eba39fdf899156e873431c9446dc56a
SK <gfawkes1832@gmail.com>
2023-09-23 22:11:22 +0530

    Spiced up landing page

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,16 +1,39 @@
 <script>
-    import { slide } from "svelte/transition";
+    import { fly } from "svelte/transition";
     import { onMount } from "svelte";
+    import { quintIn } from "svelte/easing";
+
+    $: menuIsOpen = false;
+    const handleClick = () => {
+        menuIsOpen = !menuIsOpen;
+    }
+
 </script>
 
 <section>
     <div class="img-container">
-        <img src="/g2g.png" alt="">
+        <a href="/"> 
+            <img src="/g2g.png" alt="">
+        </a>
     </div>
 
-    <div class="menu-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"/><line x1="40" y1="128" x2="216" y2="128" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="40" y1="64" x2="216" y2="64" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="40" y1="192" x2="216" y2="192" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="menu-btn" on:click={handleClick}>
+        {#if !menuIsOpen}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"/><line x1="40" y1="128" x2="216" y2="128" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="40" y1="64" x2="216" y2="64" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="40" y1="192" x2="216" y2="192" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>
+        {:else}
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#0a0a0a" viewBox="0 0 256 256"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg> 
+        {/if}
     </div>
+
+    {#if menuIsOpen}
+        <div class="menu" transition:fly={{duration: 300, easing: quintIn, x: 200}}>
+            <ul class="text-dark">
+                <li class="text-dark"> <a href="/">Dashboard</a></li>
+                <li> <a href="/">My Profile</a></li>
+            </ul>
+        </div>
+    {/if}
 </section>
 
 <style>
@@ -22,12 +45,38 @@
         @apply justify-between;
         @apply px-4;
         @apply mb-8;
+        z-index: 10;
+        @apply relative;
     }
 
     .menu-btn {
         height: 30px;
         width: 30px;
         @apply cursor-pointer;
+        @apply relative;
+        z-index: 50;
+    }
+
+    .menu {
+        @apply absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        height: 100vh;
+        width: 200px;
+        @apply bg-light;
+        @apply text-dark;
+        @apply flex;
+        @apply items-center;
+        @apply justify-center;
+    }
+
+    .menu ul {
+        @apply text-xl;
+        @apply flex;
+        @apply items-center;
+        @apply flex-col;
+        @apply gap-4;
     }
 
     .img-container img{
