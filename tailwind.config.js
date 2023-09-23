f927a61b0b465b29f96133af8770b2d6ebf2e217
SK <gfawkes1832@gmail.com>
2023-09-23 15:21:07 +0530

    Added fonts + color config— need to update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      'satoshi' : ['Satoshi-Variable', ...defaultTheme.fontFamily.sans]
+    },
+
+    fontSize: {
+      title : '48px',
+      sub: '22px',
+      body : '14px'
+    },
+
+    colors : {
+      lake: '#79BEEE',
+      indigo: '##8A84E2',
+      breeze: '#B7D3F2',
+      light: '#FAFAFA',
+      dark: "#222222",
+    }
   },
   plugins: [],
 }
